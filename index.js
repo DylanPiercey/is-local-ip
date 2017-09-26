98542a9d4a14b3076cdfb6b64249cf7dee6ce293
Quentin Burny <qburny@hubee.tv>
2017-09-26 14:46:18 +0200

    Add support for 169.254.(1-254).x (DHCP IPs) + fd00::/8 + fe80::/10

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ module.exports = isLocalIp
 // 172.16.0.0 - 172.31.255.255
 // 192.168.0.0 - 192.168.255.255
 var local = /^(::f{4}:)?(10\.\d{1,3}|127\.\d{1,3}|172\.1[6-9]|172\.2\d|172\.3[0-1]|192\.168)\.\d{1,3}\.\d{1,3}/
+// Matches
+// 169.254.1.0 - 169.254.254.255
+var dhcpV4 = /^(::f{4}:)?169\.254\.([1-9]|1?\d\d|2[0-4]\d|25[0-4])\.\d{1,3}/
+// Matches
+// fd00::/8
+var ulaV6 = /^fd[0-9a-f]{2}(::1$|:[0-9a-f]{1,4}){1,7}/
+// Matches
+// fe80::/10
+var fe80V6 = /^fe[89ab][0-9a-f](::1$|:[0-9a-f]{1,4}){1,7}/
 /**
  * Tests that an ip address is one that is reserved for local area, or internal networks.
  */
@@ -15,7 +24,9 @@ function isLocalIp (address) {
   return (
     address === '::' ||
     address === '::1' ||
-    address === 'fe80::1' ||
-    local.test(address)
+    local.test(address) ||
+    dhcpV4.test(address) ||
+    ulaV6.test(address) ||
+    fe80V6.test(address)
   )
 }
